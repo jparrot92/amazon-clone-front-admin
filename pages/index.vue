7bb112d865bbbd9da208e9839b1cfc5040bf1d71
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2021-06-26 17:26:40 +0200

    Create listing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,71 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">amazon-clone-front-admin</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <main>
+    <div class="a-spacing-large"></div>
 
-      <a href="#" class="a-button-buy-again">Buy Now</a>
+    <div class="container-fluid browsing-history">
+      <div class="row">
+        <div class="col-sm-8 col-8">
+          <h1 class="a-size-large a-spacing-none a-text-normal">
+            All products
+          </h1>
+          <div class="a-spacing-large"></div>
+          <!-- Button -->
+          <a href="#" class="a-button-buy-again">Add a new products</a>
+          <a href="#" class="a-button-history margin-right-10">
+            Add a new category
+          </a>
+          <a href="#" class="a-button-history margin-right-10">
+            Add a new owner
+          </a>
+          <!-- Listing page -->
+        </div>
+      </div>
     </div>
-  </div>
+    <div class="a-spacing-large"></div>
+    <div class="container-fluid browsing-histoty">
+      <div class="row">
+        <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+          <div class="history-box">
+            <!-- Product image -->
+            <a href="#" class="a-link-normal">
+              <img src="#" class="img-fluid" />
+            </a>
+            <!-- Product title -->
+            <div class="a-spacing-top-base asin-title">
+              <span class="a-text-normal">
+                <div class="p13n-sc-truncated">Product title</div>
+              </span>
+            </div>
+            <!-- Product rating -->
+            <div class="a-row">
+              <a href="#">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </a>
+              <span class="a-letter-space"></span>
+              <span class="a-color-tertiary a-size-small asin-reviews">
+                (1732)
+              </span>
+            </div>
+            <!-- Product priece -->
+            <div class="a-row">
+              <span class="a-size-base a-color-price">
+                <span class="p13n-sc-price">$23</span>
+              </span>
+            </div>
+            <!-- Product Buttons -->
+            <div class="a-row">
+              <a href="#" class="a-button-history margin-right-10">Update</a>
+              <a href="#" class="a-button-history margin-right-10">Delete</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
